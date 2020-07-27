e24a92b3fcd3e96b2cf8eefe385bde5b25f54087
Subham <subhambh.2000@gmail.com>
2020-07-27 14:52:35 +0530

    CRUD#3

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import todomodel
 def todoview(request):
-    return render(request,"todoapp/homepage.html")
+    myTodo = todomodel.objects.all()
+    context = {'myTodo' : myTodo}
+    return render(request,"todoapp/homepage.html",context)
+def addtask(request):
+    # function to add the input data to the database
+    mytask = request.POST['task']
+    todomodel(task=mytask).save()
+    return redirect(request.META['HTTP_REFERER'])
