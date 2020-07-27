4411398805002cc9c254c2101b26ced41c88a67a
Subham <subhambh.2000@gmail.com>
2020-07-27 15:22:18 +0530

    CRUD#3

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import todoview,addtask
+from .views import todoview,addtask,deletetask
 
 urlpatterns = [
     path('',todoview),
     path('addtask/',addtask),
+    path('delete/<int:taskid>/',deletetask),
 ]
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render,redirect
 from .models import todomodel
 def todoview(request):
     myTodo = todomodel.objects.all()
-    context = {'myTodo' : myTodo}
+    context = {'myTodo':myTodo}
     return render(request,"todoapp/homepage.html",context)
 def addtask(request):
     # function to add the input data to the database
     mytask = request.POST['task']
     todomodel(task=mytask).save()
+    return redirect(request.META['HTTP_REFERER'])
+def deletetask(request,taskid):
+    todomodel.objects.filter(id=taskid).delete()
     return redirect(request.META['HTTP_REFERER'])
