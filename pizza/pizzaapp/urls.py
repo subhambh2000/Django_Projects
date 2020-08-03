ea6ae3a9f252abd1b5b838cb9e4c93b16e6d2e01
Subham <subhambh.2000@gmail.com>
2020-08-03 22:57:41 +0530

    AdminAuthentication

--- pizza/pizzaapp/urls.py
+++ pizza/pizzaapp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import adminloginview
 
 urlpatterns = [
-    path('admin/',adminloginview),
+    path('admin/',adminloginview,name='adminloginpage'),
 ]
--- pizza/pizzaapp/views.py
+++ pizza/pizzaapp/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
 # Create your models here.
 
 def adminloginview(request):
     return render(request,"pizzaapp/adminlogin.html")
+def authenticateadmin(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(username=username, password=password)
+    # case 1: user exists
+    if user is not None:
+        login(request,user)
+
+    # case 2: user doesnot exists
+    if user is None:
+        redirect('adminloginpage')
+
+def adminhomepage(request):
+    return render(request,"pizzaapp/adminhomepage.html")
