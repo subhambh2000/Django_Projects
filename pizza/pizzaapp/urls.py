b405991cc62ce964ff75553612e1d44b925e49e7
Subham Bhattacharjee <42173825+subhambh2000@users.noreply.github.com>
2021-06-10 20:21:24 +0530

    Delete pizza directory

--- pizza/pizzaapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import adminloginview,adminhomepage,authenticateadmin
-
-urlpatterns = [
-    path('admin/',adminloginview,name='adminloginpage'),
-    path('authenticate/',authenticateadmin),
-    path('admin/homepage/',adminhomepage,name='adminhomepage'),
-]
